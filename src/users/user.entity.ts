4b4a9222aeab346bd9abbcb5aa973e3cc105aa7e
Manish Sharma <sharmakmanish1@gmail.com>
2024-01-06 16:01:11 +0530

    all routes

---
+++ src/users/user.entity.ts
@@ -0,0 +1,28 @@
+import { Exclude } from "class-transformer";
+import { AfterInsert, AfterUpdate, AfterRemove, Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    email: string;
+
+    @Column()
+    @Exclude()
+    password: string;
+
+    @AfterInsert()
+    logInsert() {
+        console.log('Inserted User with id', this.id);
+    }
+    @AfterUpdate()
+    logUpdate() {
+        console.log('Updated User with id', this.id);
+    }
+    @AfterRemove()
+    logRemove() {
+        console.log('Removed User with id', this.id);
+    }
+}
