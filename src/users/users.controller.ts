b5726b29e6714cfaafd9cd302d58c62333109d43
Manish Sharma <sharmakmanish1@gmail.com>
2024-01-13 14:34:25 +0530

    interceptor usage on module/global level

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,21 +7,19 @@ import {
     Patch,
     Post,
     Query,
-    Session,
-    UseInterceptors
+    Session
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
-import { UsersService } from './users.service';
-import { AuthService } from './auth.service';
-import { CurrentUser } from './decorators/current-user.decorator';
 import { User } from './user.entity';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+import { UsersService } from './users.service';
 @Controller('auth')
 @Serialize(UserDto)
-@UseInterceptors(CurrentUserInterceptor)
+
 export class UsersController {
     constructor(private usersService: UsersService, private authService: AuthService) { }
 
