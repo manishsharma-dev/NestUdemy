db85ad3a78432d37868ea18d06c61c5ae44800fd
Manish Sharma <sharmakmanish1@gmail.com>
2024-01-20 15:26:41 +0530

    new report create

--- src/reports/dtos/create-report.dto.ts
+++ src/reports/dtos/create-report.dto.ts
@@ -1,19 +1,40 @@
-import { IsEmail, IsString } from 'class-validator';
-
+import {
+    IsNumber,
+    IsString,
+    Min,
+    Max,
+    IsLongitude,
+    IsLatitude,
+    
+} from 'class-validator';
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
 export class CreateReportDto {
 
+    @IsString()
     make: string;
 
-
+    @IsString()
     model: string;
 
+    @IsNumber()
+    @Min(1930)
+    @Max(currentYear)
     year: number;
 
+    @IsNumber()
+    @Min(0)
+    @Max(1000000)
     mileage: number;
 
+    @IsLongitude()
     lng: number;
 
+    @IsLatitude()
     lat: number;
 
+    @IsNumber()
+    @Min(0)
+    @Max(1000000)
     price: number;
 }
